327753156e108560bfd1f993b2f14433be5bb1d3
SanjayKumar <0508sanjaykumar@gmail.com>
2024-04-14 13:11:51 +0530

    first commit

--- Lecture 1/Sample.java
+++ Lecture 1/Sample.java
@@ -1,19 +1,7 @@
-import java.util.Scanner;
 public class Sample
 {
     public static void main(String[] args) 
     {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("");
-        int x = sc.nextInt();
-        if(x%2 == 0)
-        {
-            System.out.println("YES");
-        }
-        else
-        {
-            System.out.println("NO");
-        }
-        sc.close();
+        System.out.println("Hello World");
     }
 }
